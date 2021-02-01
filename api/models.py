fb3a336f1aa536a8339ed2f49662a2e086ca6b90
Ayush <verma16.ayush@gmail.com>
2021-02-01 18:08:39 +0530

    added models for prof, stud, comment

--- api/models.py
+++ api/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
-# Create your models here.
+class Professor(models.Model):
+    DEPT_CHOICES = (
+        ('archi', 'architecture' ),
+        ('msc', 'material-science-engineering'),
+        ('chemee', 'chemical-engineering'),
+        ('che', 'chemistry'),
+        ('ce', 'civil-engineering'),
+        ('cse', 'computer-science-engineering'),
+        ('ee', 'electrical-engineering'),
+        ('ece', 'electronics-communication-engineering'),
+        ('hs', 'humanities-social-sciences'),
+        ('math', 'mathematics'),
+        ('mech', 'mechanical-engineering'),
+        ('phy', 'physics',),
+        ('man', 'management-studies'),
+    )
+
+    RATING_CHOICES = (
+        (0, 'worst'),
+        (1, 'bad'),
+        (2, 'ok'),
+        (3, 'nice'),
+        (4, 'good'),
+        (5, 'best'),
+    )
+    name = models.CharField(max_length=200, blank=True, null=True)
+    dept = models.CharField(max_length=200, blank=True, null=True, choices=DEPT_CHOICES)
+    rating = models.IntegerField(default=0, null=False, blank=False, choices=RATING_CHOICES)
+    designation = models.CharField(max_length=200, null=True, blank=True)
+    email = models.EmailField(max_length=200, null=True, blank=True)
+    portfolio_url = models.URLField(max_length=800, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Student(models.Model):
+    username = models.CharField(max_length=200, null=True, blank=False)
+    email = models.CharField(max_length=200, null=True, blank=True)
+    karma = models.IntegerField(null=False, blank=False, default=0)
+
+    def __str__(self):
+        return self.username
+
+
+class Comment(models.Model):
+    prof = models.ForeignKey(Professor, null=True, on_delete=models.CASCADE)
+    stud = models.ForeignKey(Student, null=True, on_delete=models.CASCADE)
+    content = models.TextField(max_length=1500, null=True, blank=True, editable=True)
+    date_added = models.DateTimeField(auto_now_add=True, null=True)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.stud.username + '\'s review of ' + self.prof.name

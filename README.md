9bb0a66e05f7ab76a04b04ab0066ba21b914e041
Ayush <verma16.ayush@gmail.com>
2021-02-07 16:32:51 +0530

    add Readme

---
+++ README.md
@@ -0,0 +1,3 @@
+This is The backendd part of our project
+
+To view the flutter frontend of ProfPoll visit : (link)[https://github.com/rv299792458/profpoll_front]
--- profpoll/settings.py
+++ profpoll/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'api.apps.ApiConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'profpoll.urls'
@@ -121,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+ALLOWED_HOSTS=['*']
+CORS_ORIGIN_ALLOW_ALL = True

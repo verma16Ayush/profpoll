58270dd8c4bb839435489819af06bb4f3f139ebf
Ayush <verma16.ayush@gmail.com>
2021-06-25 21:54:35 +0530

    deployed finally

--- profpoll/settings.py
+++ profpoll/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '(1h9!va(ey%$#ww^fj%8s=q@b!f10(5nsv(x%-98^dl@762n7b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '192.168.43.189']
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.43.189', 'https://profpoll.herokuapp.com/']
 # fdfj
 
 # Application definition
